a53aeee90ad5fd62be102a2227f99196e53ed4cb
Alexander <alexmezhenskyi@gmail.com>
2017-05-14 21:03:09 +0300

    refactored

--- assets/scripts/map/map.js
+++ assets/scripts/map/map.js
@@ -1,21 +1,21 @@
 function initMap() {
-	let coordinates = {lat: 48.7345164, lng: 37.579577};
-	let mapContainer = document.querySelector('#map');
-	let map = new google.maps.Map(mapContainer, {
+	var coordinates = {lat: 48.7345164, lng: 37.579577};
+	var mapContainer = document.querySelector('#map');
+	var map = new google.maps.Map(mapContainer, {
 		center: coordinates,
 		zoom: 15,
 		disableDefaultUI: true,
 		scrollwheel: false
 	});
 
-	let marker = new google.maps.Marker({
+	var marker = new google.maps.Marker({
 		position: coordinates,
 		map: map,
 		icon: "assets/img/map-marker.png"
 	});
 
 	google.maps.event.addDomListener(window, "resize", function () {
-		let center = map.getCenter();
+		var center = map.getCenter();
 		google.maps.event.trigger(map, "resize");
 		map.setCenter(center);
 	})
